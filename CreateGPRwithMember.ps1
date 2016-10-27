525f4918e04ccc3d256f136403679ab8b25d4a97
AdrienN <aneel@net-online.fr>
2016-10-27 15:20:54 +0200

    Script modifié

---
+++ CreateGPRwithMember.ps1
@@ -0,0 +1,18 @@
+﻿Param(
+  [string]$groupList,
+  [string]$groupMemberList,
+  [string]$destinationDN
+)
+
+#Get-Content $groupList | Set-Content -Encoding utf8 $groupList-utf8
+Import-CSV $groupList | Foreach{
+    $oldname = $_.oldGroup
+    $name = $_.groupName
+    $membercn = $_.memberCN
+    $memberadd = $membercn.Split("{,}")
+    #Write-Host "${dn},${destinationDN}"
+    New-ADGroup -Name "$name" -SamAccountName $name -GroupCategory Security -GroupScope Global -DisplayName "$name" -Path "OU=Groupes-AD,DC=tephadet,DC=lan" -Description "Groupe $oldname dans eDirectrory"
+    Add-ADGroupMember -identity ${name} -Member $memberadd
+
+    
+}
