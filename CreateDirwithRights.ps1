6b6c5ff3e2996fbffe09d24b76ce4ed24df4cd87
AdrienN <aneel@net-online.fr>
2016-10-27 15:41:11 +0200

    add script

---
+++ CreateDirwithRights.ps1
@@ -0,0 +1,26 @@
+﻿Param(
+  [string]$rightsList,
+  [string]$rootPath,
+  [string]$domain
+)
+
+Import-Module NTFSSecurity
+Get-Content $rightsList | Set-Content -Encoding utf8 $rightsList-utf8
+
+Import-Csv $rightsList-utf8 | Foreach {
+    $upn = $_.uid
+    $directory = $_.path
+    $rights = $_.trustee
+    $appliesto = $_.appliesto
+    
+    Write-Host New-Item -ItemType Directory -Force -Path "${rootPath}\${directory}"
+    try {
+        New-Item -ItemType Directory -Force -Path "${rootPath}\${directory}"
+        Add-NTFSAccess -Account "${domain}\${upn}" -Path "${rootPath}${directory}" -AccessRights "${rights}" -AppliesTo "${appliesto}"     
+    }
+    catch
+    {
+      "Error when Create directory or adding right for ${upn} on ${directory}." | Out-File .\Adddirectory.log -Append
+      "$_.Message" | Out-File .\adddirectory.log -Append
+    }
+}
