a1397e594436a8bee3a30e48db2919bddf33c285
Thierry Rangeard <trangeard@net-online.fr>
2016-10-28 10:52:28 +0200

    Files moved to directories

---
+++ CleanCreateHomeDir/Clean4HomeDir.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+# @Author: Thierry Rangeard <Gandalf>
+# @Date:   16-Oct-2016
+# @Email:  trangeard@net-online.fr
+# @Project: CreateHomeDir
+# @Fonction: nettoyage et ajout au bon format pour utiliser CreateDirwithRigths.ps1
+# @Last modified by:   AdrienN
+# @Last modified time: 23-Oct-2016
+
+import sys, getopt
+reload(sys);
+sys.setdefaultencoding("utf8")
+import re
+
+def main(argv):
+  inputfile = ''
+  try:
+     opts, args = getopt.getopt(argv,"hi:",["ifile="])
+  except getopt.GetoptError:
+     print 'Clean_Grp.py -i <inputfile>'
+     sys.exit(2)
+  for opt, arg in opts:
+     if opt == '-h':
+        print 'Clean_Grp.py -i <inputfile> un fichier en entrée <inputfile> et un en sortie out.csv'
+        sys.exit()
+     elif opt in ("-i", "--ifile"):
+        inputfile = arg
+  print 'Le fichier traité est :', inputfile
+  return()
+
+def CleanHomeDir(input_file, output_file):
+   with open (input_file, 'r') as i:
+       content = i.readlines()
+       with open (output_file, 'w') as o:
+           for lines in content:
+               print (lines)
+               out = re.sub(r'^\"CN=', "", lines)
+               out1 = re.sub('[,].*', "", out)
+               out11 = re.sub("\n", "",out1)
+               out2 = out11 + ',\\' + out11 + ',\"Read,ReadAndExecute,Synchronize,Write,CreateFiles,CreateDirectories,Delete,Modify,ListDirectory\",ThisFolderSubFoldersAndFiles\n'
+               o.write(out2)
+   return()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+   CleanHomeDir(sys.argv[1],'CleanHomedir-out.csv')
---
+++ CleanCreateHomeDir/CreateDirwithRights.ps1
@@ -0,0 +1,34 @@
+﻿# @Author: Adrien Neel <AdrienN>
+# @Date:   27-Oct-2016
+# @Email:  aneel@net-online.fr
+# @Project: CreateHomDir
+# @Fonction: cree des repertoires en ajoutant des droits via un fichiers csv en entree
+# @Last modified by:   AdrienN
+# @Last modified time: 27-Oct-2016
+
+Param(
+  [string]$rightsList,
+  [string]$rootPath,
+  [string]$domain
+)
+
+Import-Module NTFSSecurity
+Get-Content $rightsList | Set-Content -Encoding utf8 $rightsList-utf8
+
+Import-Csv $rightsList-utf8 | Foreach {
+    $upn = $_.uid
+    $directory = $_.path
+    $rights = $_.trustee
+    $appliesto = $_.appliesto
+    
+    Write-Host New-Item -ItemType Directory -Force -Path "${rootPath}\${directory}"
+    try {
+        New-Item -ItemType Directory -Force -Path "${rootPath}\${directory}"
+        Add-NTFSAccess -Account "${domain}\${upn}" -Path "${rootPath}${directory}" -AccessRights "${rights}" -AppliesTo "${appliesto}"     
+    }
+    catch
+    {
+      "Error when Create directory or adding right for ${upn} on ${directory}." | Out-File .\Adddirectory.log -Append
+      "$_.Message" | Out-File .\adddirectory.log -Append
+    }
+}
