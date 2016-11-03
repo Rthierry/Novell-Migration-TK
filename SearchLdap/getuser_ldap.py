523bc9f8135709dbeba166f59f348a5a9545bd71
Thierry Rangeard <trangeard@net-online.fr>
2016-11-03 23:20:33 +0100

    ident

--- SearchLdap/getuser_ldap.py
+++ SearchLdap/getuser_ldap.py
@@ -7,51 +7,29 @@
 # @Last modified by:   FullTacos
 # @Last modified time: 24-Oct-2016
 
+import argparse
 import sys
 import ldap
 
-def  main(argv):
-    servername = ''
-    username = ''
-    password = ''
-    dnbase = ''
-    try:
-        opts, args = getopt.getopt(argv,"hs:u:p:d:",["help","server_ldap=","username=","password=","dn_base="])
-    except getopt.GetoptError:
-        print 'getuser_ldap.py -s <Ldap_Serveur_Adresse>'
-        sys.exit(2)
-        for opt, arg in opts:
-            if opt in ("-h","--help"):
-                print ("getuser_ldap.py -s adresse du serveur ldap")
-                sys.exit()
-            elif opt in ("-s","--server_ldap"):
-                servername = arg
-            elif opt in ("-u", "--username"):
-                username = arg
-            elif opt in ("-p", "--password"):
-                password = arg
-            elif opt in ("-d", "--dn_base"):
-                dnbase = arg
-            else:
-                assert False, "unhandled option"
-        print 'Le traitement recuperation'
-        print (servername)
+parser = argparse.ArgumentParser()
+parser.add_argument("servername", type=str, help=" -> serverldap")
+parser.add_argument("username", type=str, help=" -> username")
+parser.add_argument("password", type=str, help=" -> password")
+parser.add_argument("dnbase", type=str, help=" -> dnbase")
+
+
+args = parser.parse_args()
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
-   servername = str(sys.argv[2:3])
-   username = str(sys.argv[4:5])
-   password = str(sys.argv[6:7])
-   dnbase = str(sys.argv[8:9])
-   print (servername,username,password,dnbase)
-   # adjust this to your base dn for searching
-   connect = ldap.open('ldap://'servername':389')
-   try:
-       #if authentication successful, get the full user data
-       connect.bind_s(username,password)
-       r = connect.search_s(dnbase, ldap.SCOPE_SUBTREE, '(|(objectClass=User)(objectClass=Person)(objectClass=inetOrgPerson))', ['cn', 'mail'])
-       for dn,entry in r:
-          print 'Processing', repr(dn)
-   except ldap.LDAPError:
-       connect.unbind_s()
-       print "authentication error"
+    print (servername, username, password, dnbase)
+    # adjust this to your base dn for searching
+    connect = ldap.open('ldap://'+servername+':389')
+    try:
+        # if authentication successful, get the full user data
+        connect.bind_s(username, password)
+        r = connect.search_s(dnbase, ldap.SCOPE_SUBTREE, '(|(objectClass=User)(objectClass=Person)(objectClass=inetOrgPerson))', ['cn', 'mail'])
+        for dn, entry in r:
+            print 'Processing', repr(dn)
+        except ldap.LDAPError:
+            connect.unbind_s()
+            print "authentication error"
