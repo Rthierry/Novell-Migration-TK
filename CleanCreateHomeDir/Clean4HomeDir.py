f229fc49d89025b0053c67c8eb5899d360747c93
Thierry Rangeard <trangeard@net-online.fr>
2016-11-03 22:58:40 +0100

    ident correction

--- CleanCreateHomeDir/Clean4HomeDir.py
+++ CleanCreateHomeDir/Clean4HomeDir.py
@@ -9,40 +9,30 @@
 # @Last modified by:   AdrienN
 # @Last modified time: 23-Oct-2016
 
-import sys, getopt
-reload(sys);
-sys.setdefaultencoding("utf8")
+import sys
 import re
+import argparse
+reload(sys)
+sys.setdefaultencoding("utf8")
 
-def main(argv):
-  inputfile = ''
-  try:
-     opts, args = getopt.getopt(argv,"hi:",["ifile="])
-  except getopt.GetoptError:
-     print 'Clean_Grp.py -i <inputfile>'
-     sys.exit(2)
-  for opt, arg in opts:
-     if opt == '-h':
-        print 'Clean_Grp.py -i <inputfile> un fichier en entrée <inputfile> et un en sortie out.csv'
-        sys.exit()
-     elif opt in ("-i", "--ifile"):
-        inputfile = arg
-  print 'Le fichier traité est :', inputfile
-  return()
 
-def CleanHomeDir(input_file, output_file):
-   with open (input_file, 'r') as i:
-       content = i.readlines()
-       with open (output_file, 'w') as o:
-           for lines in content:
-               print (lines)
-               out = re.sub(r'^\"CN=.*.\"[,]\"', "", lines)
-               out1 = re.sub(r'\"', "", out)
-               out12 = re.sub("\r\n", "",out1)
-               out2 = out12 + ",\\" + out12 + ",\"Read,ReadAndExecute,Synchronize,Write,CreateFiles,CreateDirectories,Delete,Modify,ListDirectory\",ThisFolderSubFoldersAndFiles\n"
-               o.write(out2)
-   return()
+parser = argparse.ArgumentParser()
+parser.add_argument("input_file", type=str, help=" -> Fichier trustee liste.txt")
+
+args = parser.parse_args()
+
+def  CleanHomeDir(input_file, output_file):
+    with open(input_file, 'r') as i:
+        content = i.readlines()
+        with open(output_file, 'w') as o:
+            for lines in content:
+                print (lines)
+                out = re.sub(r'^\"CN=.*.\"[,]\"', "", lines)
+                out1 = re.sub(r'\"', "", out)
+                out12 = re.sub("\r\n", "", out1)
+                out2 = out12 + ",\\" + out12 + ",\"Read,ReadAndExecute,Synchronize,Write,CreateFiles,CreateDirectories,Delete,Modify,ListDirectory\",ThisFolderSubFoldersAndFiles\n"
+                o.write(out2)
+    return()
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
-   CleanHomeDir(sys.argv[1],'CleanHomedir-out.csv')
+    CleanHomeDir(input_file, 'CleanHomedir-out.csv')
