5f2e3b5718e77b57c26c1d232ccdbcde6483103d
Thierry Rangeard <trangeard@net-online.fr>
2016-11-03 22:59:21 +0100

    ident correction

--- SearchLdap/ldapReqADedir.py
+++ SearchLdap/ldapReqADedir.py
@@ -6,12 +6,12 @@
 # @Project: Utilitaire de lecture ldap
 # @Last modified by:   Gandalf
 # @Last modified time: 26-Oct-2016
-
-import sys, getopt
-reload(sys);
+import ldap
+import sys
+reload(sys)
 sys.setdefaultencoding("utf8")
 
-import ldap
+
 Server = "ldap://172.16.48.220"
 
 BaseAD = "dc=netonline,dc=loc"
@@ -21,7 +21,7 @@ FilterAD = "(&(objectClass=user)(!(objectClass=computer)))"
 FiltereDir = ""
 Attrs = ['SamAccountName']
 
-l =ldap.initialize(Server)
+l = ldap.initialize(Server)
 l.protocol_version = 3
 l.set_option(ldap.OPT_REFERRALS, 0)
 l.simple_bind_s('Administrateur@netonline.loc', 'Netonline@26')
