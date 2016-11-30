568bfa7b88a6767ac1ee88c671f172bd014765f5
Thierry Rangeard <trangeard@net-online.fr>
2016-11-30 23:32:29 +0100

    Version ldaps

--- SearchLdap/ldapReqADedir.py
+++ SearchLdap/ldapReqADedir.py
@@ -7,24 +7,59 @@
 # @Last modified by:   Gandalf
 # @Last modified time: 26-Oct-2016
 import ldap
+import getopt
 import sys
 reload(sys)
 sys.setdefaultencoding("utf8")
 
 
-Server = "ldap://172.16.48.220"
+def main(argv):
+    inputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:",["ifile="])
+    except getopt.GetoptError:
+        print 'ldapReqADedir.py -s <ldapserver> -b <base> -'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'nss2csv.py -i <inputfile.xml> deux fichiers en sortie _trustee.csv et _quota.csv'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        print 'Le fichier traité est :', inputfile
+    return()
+
+#Accept Self-Sign Certificates
+ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+Server = "ldaps://172.30.254.44"
 
 BaseAD = "dc=netonline,dc=loc"
-BaseeDir = "O=netonline"
+BaseeDir = "O=stac"
 Scope = ldap.SCOPE_SUBTREE
 FilterAD = "(&(objectClass=user)(!(objectClass=computer)))"
-FiltereDir = ""
-Attrs = ['SamAccountName']
-
+FiltereDir = "(&(objectClass=user))"
+Attrs = ['loginScript']
 l = ldap.initialize(Server)
 l.protocol_version = 3
 l.set_option(ldap.OPT_REFERRALS, 0)
-l.simple_bind_s('Administrateur@netonline.loc', 'Netonline@26')
-r = l.search_s(BaseAD, Scope, FilterAD, Attrs)
+l.simple_bind_s('cn=admin,ou=94,o=stac', 'root_root')
+
+r = l.search_s(BaseeDir, Scope, FiltereDir, Attrs)
+
 for dn in r:
     print 'Processing', repr(dn)
+
+#mod_attrs = [(ldap.MOD_ADD,'loginScript', 'toto' )]
+#l.modify_s('cn=testmac,o=stac', (ldap.MOD_ADD,attr,''))
+#l.modify_s('cn=testmac,o=stac', mod_attrs)
+
+def GetLdapMod(dn, attr, value):
+    mod_attrs = [(ldap.MOD_DELETE, attr, None )]
+    l.modify_s(dn, mod_attrs)
+    mod_attrs = [(ldap.MOD_ADD, attr, value)]
+    l.modify_s(dn, mod_attrs)
+    return
+
+if __name__ == "__main__":
+    #GetLdapMod('cn=testmac,o=stac', 'loginScript', 'test depuis python')
+    GetLdapMod('cn=testmac,o=stac', 'loginScript', 'tutu')
